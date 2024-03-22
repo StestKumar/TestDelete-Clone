937f80889fb29dbe807e96e096958f2ffc9eb0d6
Prashant Kumar <softwaretesterKumar@gmail.com>
2024-03-22 15:19:50 +0000

    Added int

---
+++ src/Testing.java
@@ -0,0 +1,16 @@
+public class Testing
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println("Calculate the value of =" + (a+c)/b);
+
+        System.out.println();
+    }
+
+
+
+}
